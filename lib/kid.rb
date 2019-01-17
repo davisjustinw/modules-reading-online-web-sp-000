7ce1a30758101c8b91639db064a1d1eb25d130a8
Justin Davis <davisjustinw@gmail.com>
2019-01-17 23:08:59 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -4,6 +4,9 @@ require_relative './dance_module.rb'
 
 class Kid
   attr_accessor :name
+  def initialize(name)
+      @name = name
+  end 
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
 end
